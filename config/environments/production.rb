b57b9f2a3fabed9b2d2bf0ebefa55c3208a6f087
rathorsonu <sunnysonurathor2016@gmail.com>
2024-06-03 14:31:35 +0530

    resolve db error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "demo_production"
 
   config.action_mailer.perform_caching = false
-
+  config.active_record.sqlite3_production_warning=false
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
